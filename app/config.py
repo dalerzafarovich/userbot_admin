0f8a3a53cfe7c8472d95e65ebe9b398d904d34cd
dalerzafarovich <48939169+dalerzafarovich@users.noreply.github.com>
2021-07-26 18:44:17 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
-channel_id = 'Hotline_Ufa'  # id либо юзернейм канала
-bot_token = '1069704894:AAFADsPveToRqnINmwHmz5xjrBRXs7sPWaU'  # токен бота
-bot_name = 'diamondssvolkbot'  # юзернейм бота
+channel_id = ''  # id либо юзернейм канала
+bot_token = ''  # токен бота
+bot_name = ''  # юзернейм бота
 
-userbot_id = 1822239704  # не менять
+userbot_id =   #id юзербота
 
 # настройки Redis:
 redis_config = {
-    'host': 'redis-16510.c276.us-east-1-2.ec2.cloud.redislabs.com',
-    'port': 16510,
-    'password': '8V3NwI1D7f21zVSbIcOuoSgvFzZSEABU'
+    'host': '',
+    'port': ,
+    'password': ''
 }
